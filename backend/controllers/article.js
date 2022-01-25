c8ecce60c4512d7fef83fa03dca521852b9ff637
Angel-Mosqueda <Angel_Mosqueda_Esparza@hotmail.com>
2022-01-24 19:34:11 -0600

    se pueden subir imagenes en postman

--- backend/controllers/article.js
+++ backend/controllers/article.js
@@ -134,6 +134,129 @@ var controller = {
             });
         })
 
+    },
+
+    update: (req, res) => {
+        //Recoger el id del articulo por la url
+        var articleId = req.params.id;
+
+        //Recoger los datos que llegan por put
+        var params = req.body;
+
+        //Validar datos
+        try {
+            var validate_title = !validator.isEmpty(params.title);
+            var validate_content = !validator.isEmpty(params.content);
+        } catch (err) {
+            return res.status(404).send({
+                status: 'error',
+                message: 'Faltan datos por enviar'
+            });
+        }
+
+        if (validate_title && validate_content) {
+            //Find and update
+            Article.findOneAndUpdate({ _id: articleId }, params, { new: true }, (err, articleUpdated) => {
+                if (err) {
+                    return res.status(500).send({
+                        status: 'error',
+                        message: 'Error al actualizar'
+                    });
+                }
+
+                if (!articleUpdated) {
+                    return res.status(404).send({
+                        status: 'error',
+                        message: 'No existe el articulo !!!'
+                    });
+                }
+
+                return res.status(200).send({
+                    status: 'succes',
+                    article: articleUpdated
+                });
+
+            });
+        } else {
+            return res.status(500).send({
+                status: 'error',
+                message: 'La validacion no es correcta'
+            });
+        }
+
+
+
+
+
+    },
+
+    delete: (req, res) => {
+        //Recoger el id de la url
+        var articleId = req.params.id;
+
+        Article.findOneAndDelete({ _id: articleId }, (err, articleRemoved) => {
+            if (err) {
+                return res.status(500).send({
+                    status: 'error',
+                    message: 'Error al borrar'
+                });
+            }
+
+            if (!articleRemoved) {
+                return res.status(404).send({
+                    status: 'error',
+                    message: 'No se ha borrado el articulo, posiblemente no exista'
+                });
+            }
+
+            return res.status(200).send({
+                status: 'succes',
+                article: articleRemoved
+            })
+
+        });
+    },
+
+    upload: (req, res) => {
+        //Configurar el modulo del connect multiparty: router/article.js (hecho)
+
+        //Recoger el fichero de la peticion
+        var file_name = 'Imagen no subida...';
+
+        if (!req.files) {
+            return res.status(404).send({
+                status: 'error',
+                message: file_name
+            });
+        }
+        //Conseguir el nombre y la extension del archivo
+        var file_path = req.files.file0.path;
+        var file_split = file_path.split('\\');
+
+        //ADVERTIENCIA PARA LINUX O MAC
+        // var file_split = file_path.split('/');
+
+        //Nombre del archivo
+        var file_name = file_split[2];
+
+        //Extension del fichero
+        var extension_split = file_name.split('\.');
+        var file_ext = extension_split[1];
+
+        //Comprobar la extension, solo imagenes, si es valida borrar el fichero
+        if (file_ext != 'png' && file_ext != 'jpg' && file_ext != 'jpeg' && file_ext != 'gif') {
+            //borrar el archivo subido
+        } else {
+
+        }
+
+        //Buscar el articulo, asignarle el nombre de la imagen y actualizarlo
+
+        return res.status(404).send({
+            fichero: req.files,
+            split: file_split,
+            file_ext
+        });
     }
 
 
--- backend/routes/article.js
+++ backend/routes/article.js
@@ -5,6 +5,9 @@ var ArticleController = require('../controllers/article');
 
 var router = express.Router();
 
+var multipart = require('connect-multiparty');
+var md_upload = multipart({uploadDir: './upload/articles'});
+
 //Rutas de prueba
 router.post('/datos-curso',ArticleController.datosCurso);
 router.get('/test-de-controlador',ArticleController.test);
@@ -13,5 +16,8 @@ router.get('/test-de-controlador',ArticleController.test);
 router.post('/save',ArticleController.save);
 router.get('/articles/:last?',ArticleController.getArticles);
 router.get('/article/:id',ArticleController.getArticle);
+router.put('/article/:id',ArticleController.update);
+router.delete('/article/:id',ArticleController.delete);
+router.post('/upload-image/:id',md_upload ,ArticleController.upload);
 
 module.exports = router;
